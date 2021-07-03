3fba1f295afe395981cc1c1abc689ec0b2eca36d
Delroy Brown <delroybrown.db@gmail.com>
2021-07-03 03:57:39 +0100

    Added ajaz search funtionality

--- blog/forms.py
+++ blog/forms.py
@@ -29,7 +29,7 @@ class PostSearchForm(forms.Form):
     q = forms.CharField()
     c = forms.ModelChoiceField(
         queryset=Category.objects.all().order_by('name'))
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['c'].label = ''
@@ -37,5 +37,7 @@ class PostSearchForm(forms.Form):
         self.fields['c'].label = 'Category'
         self.fields['q'].label = 'Search For'
         self.fields['q'].widget.attrs.update(
-            {'class': 'form-control'})
+            {'class': 'form-control menudd'})
+        self.fields['q'].widget.attrs.update(
+            {'data-toggle': 'dropdown'})
     
